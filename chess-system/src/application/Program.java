f0746be233ba0d08a009a5530c73651b3534c008
vitor-hc <77215009+vitor-hc@users.noreply.github.com>
2021-05-14 22:13:24 -0300

    Add files via upload
    Projeto em desenvolvimento.

---
+++ chess-system/src/application/Program.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import boardgame.Position;
+
+/**
+ *
+ * @author Vitor Hugo
+ */
+public class Program {
+
+    public static void main(String[] args)
+    {
+        
+        Position pos=new Position(3,5);
+        System.out.println(pos);
+                
+                
+                
+       
+               
+               
+    }
+    
+}
