bc94e058870981d02f7c952bdb0b1c877d560ce2
vitor-hc <77215009+vitor-hc@users.noreply.github.com>
2021-05-27 00:08:31 -0300

    Add files via upload

---
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication15;
+
+/**
+ *
+ * @author Vitor Hugo
+ */
+public class JavaApplication15 {
+
+    JavaApplication15 a1 = new JavaApplication15();
+    private static float n1;
+    private static float n2;
+    private static float n3;
+
+    public JavaApplication15(float n1, float n2, float n3) {
+
+        n1 = n1;
+
+        n1 = n1;
+
+        n1 = n3;
+
+    }
+
+    public static float media() {
+
+        return (n1 + n2 + n3) / 3;
+
+    }
+
+    public static float getN1() {
+
+        return n1;
+
+    }
+
+}
